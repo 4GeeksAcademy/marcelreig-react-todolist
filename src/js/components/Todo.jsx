f8c7d861c9519f7cdf8575088a26d3423195aa22
Marcel Reig <marcelreig@gmail.com>
2025-06-15 10:58:08 +0000

    edit styles

--- src/js/components/Todo.jsx
+++ src/js/components/Todo.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 //create your first component
 const Todo = () => {
-
   // Crear un estado para almacenar los elementos de la lista de tareas
   const [todos, setTodos] = React.useState([]);
   // Crear un estado para almacenar el valor del input
@@ -16,42 +15,48 @@ const Todo = () => {
   };
   // Crear una función para manejar la eliminación de un elemento de la lista de tareas
   const removeTodo = (index) => {
-    const newTodos = todos.filter((_, i) => i !== index);
+    const newTodos = todos.filter((element, i) => i !== index);
     setTodos(newTodos);
   };
 
-	return (
-		<div className="text-center w-25 mx-auto mt-5">
-			<h1 className="text-center mt-5 lead">Todolist de Marcel</h1>
-      <input
-        type="text"
-        className="form-control mt-3 mb-3"
-        placeholder="Añadir nueva tarea"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            addTodo();
-          }
-        }}
-      />
-      <button className="btn btn-primary mb-3" onClick={addTodo}>
-        Añadir Tarea
-      </button>
-      <ul className="list-group">
-        {todos.map((todo, index) => (
-          <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
-            {todo}
-            <button className="btn btn-danger" onClick={() => removeTodo(index)}>
-              <i class="bi bi-trash"></i>
-            </button>
-          </li>
-        ))}
-      </ul>
-      <p className="mt-3">Total Todos: {todos.length}</p>
-      
-		</div>
-	);
+  return (
+    <div className="todo-app">
+      <h1 className="text-center mt-5 todo-app__title">todos</h1>
+      <div className="todo-app__box mx-auto">
+        <input
+          type="text"
+          className="todos__input"
+          placeholder="Añadir nueva tarea"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              addTodo();
+            }
+          }}
+        />
+        <ul className="todo-list">
+          {todos.map((todo, index) => (
+            <li
+              key={index}
+              className="todo-list__item d-flex justify-content-between align-items-center"
+            >
+              <span className="todo-list__text">{todo}</span>
+              <span
+                className="todo-list__remove"
+                onClick={() => removeTodo(index)}
+              >
+                <i class="fa-solid fa-xmark"></i>
+              </span>
+            </li>
+          ))}
+        </ul>
+        <div className="todo-app__footer">
+          Tareas pendientes: {todos.length}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
